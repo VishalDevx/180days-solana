20f13cac415eb0758ec95ef75897030c355e62e1
VishalDevx <vishalcsx@gmail.com>
2025-10-22 18:14:44 +0530

    its the day one and i have written the hello world program in the rust

---
+++ day-1/target/debug/day-1.d
@@ -0,0 +1,1 @@
+/home/vishalcsx/Developer/Learning/180days-Rust/day-1/target/debug/day-1: /home/vishalcsx/Developer/Learning/180days-Rust/day-1/src/main.rs
---
+++ day-1/target/debug/deps/day_1-a9e114ddc3a9b0e7.d
@@ -0,0 +1,5 @@
+/home/vishalcsx/Developer/Learning/180days-Rust/day-1/target/debug/deps/day_1-a9e114ddc3a9b0e7.d: src/main.rs
+
+/home/vishalcsx/Developer/Learning/180days-Rust/day-1/target/debug/deps/day_1-a9e114ddc3a9b0e7: src/main.rs
+
+src/main.rs:
