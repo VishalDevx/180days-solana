7e262112670569cbdb9a43772d9051bcc91bf32f
VishalDevx <vishalcsx@gmail.com>
2025-10-28 10:11:01 +0530

    learning about the rust variables

---
+++ variables/target/debug/deps/variables-5d7aec2612ccb024.d
@@ -0,0 +1,5 @@
+/home/vishalcsx/Developer/Learning/180days-Rust/variables/target/debug/deps/variables-5d7aec2612ccb024.d: src/main.rs
+
+/home/vishalcsx/Developer/Learning/180days-Rust/variables/target/debug/deps/libvariables-5d7aec2612ccb024.rmeta: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/deps/variables-e66814e55f94f17f.d
@@ -0,0 +1,5 @@
+/home/vishalcsx/Developer/Learning/180days-Rust/variables/target/debug/deps/variables-e66814e55f94f17f.d: src/main.rs
+
+/home/vishalcsx/Developer/Learning/180days-Rust/variables/target/debug/deps/libvariables-e66814e55f94f17f.rmeta: src/main.rs
+
+src/main.rs:
