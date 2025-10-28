300c021df33374d6e75fb7786174ffacaab31834
VishalDevx <vishalcsx@gmail.com>
2025-10-28 10:22:45 +0530

    learnig the the ownership

---
+++ owenership/target/debug/deps/owenership-05c90ceb827b3b82.d
@@ -0,0 +1,5 @@
+/home/vishalcsx/Developer/Learning/180days-Rust/owenership/target/debug/deps/owenership-05c90ceb827b3b82.d: src/main.rs
+
+/home/vishalcsx/Developer/Learning/180days-Rust/owenership/target/debug/deps/libowenership-05c90ceb827b3b82.rmeta: src/main.rs
+
+src/main.rs:
---
+++ owenership/target/debug/deps/owenership-8861f55ae65a956d.d
@@ -0,0 +1,5 @@
+/home/vishalcsx/Developer/Learning/180days-Rust/owenership/target/debug/deps/owenership-8861f55ae65a956d.d: src/main.rs
+
+/home/vishalcsx/Developer/Learning/180days-Rust/owenership/target/debug/deps/libowenership-8861f55ae65a956d.rmeta: src/main.rs
+
+src/main.rs:
